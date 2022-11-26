22a4e12a230e55d00b551f99bd5a8b61e7f180f8
cullymw <cullen.mwagner@gmail.com>
2022-11-26 16:11:39 -0600

    Add empty components and routing for navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavHeaderComponent } from './nav-header/nav-header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { MenuComponent } from './menu/menu.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavHeaderComponent
+    NavHeaderComponent,
+    HomeComponent,
+    AboutUsComponent,
+    MenuComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule,
