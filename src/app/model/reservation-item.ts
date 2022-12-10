5f60668dd6e1cfc519611758bda36d8c5aae5c4a
ellaswysell <66079270+ellaswysell@users.noreply.github.com>
2022-12-09 22:40:37 -0600

    display available and confirmed reservations

---
+++ src/app/model/reservation-item.ts
@@ -0,0 +1,6 @@
+export interface IReservationItem {
+    id: number,
+    time: string,
+    name: string,
+    number: number
+}
--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { IReservationItem } from '../model/reservation-item'; 
+import { ReservationService } from '../shared/reservation.service';
+import { HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.scss']
 })
-export class ReservationComponent {
+
+export class ReservationComponent implements OnInit {
+  
+  public availableTimes: IReservationItem[] = [];
+  public reservedTimes: IReservationItem[] = [];
+  public dataLoading: boolean = false;
+  public personName: string = "";
+  public partyNumber: number;
+  public selectedReservation: IReservationItem;
+  saveUnsuccessful: boolean;
+
+  constructor(
+    public route: ActivatedRoute,
+    public reservationService: ReservationService,
+  ) {
+    // this.route.paramMap.subscribe({
+    //   next: (params) => {
+    //     this.viewTitle = params.get("view");
+    //     this.assignMenuToDisplay();
+    //   }
+    // });
+  }
+
+  ngOnInit(): void {
+    this.dataLoading = true;
+    this.reservationService.fetchReservationData()
+      .subscribe({
+        next: (data) => {
+          this.availableTimes = data.available;
+          this.reservedTimes = data.reserved;
+          this.dataLoading = false;
+        },
+        error: (err) => {
+          console.error("Error occurred: " + err);
+        }
+      });
+  }
+
+  onSubmit(ngForm: NgForm) {
+    this.saveUnsuccessful = false;
+
+    if (!ngForm.valid) {
+      this.saveUnsuccessful = true;
+      return;
+    }
+
+    console.log(ngForm);
+
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }),
+    };
+
+
+
+    ngForm.resetForm();
+
+  }
+
+  public hoveredItem: any;
+
+  public highlightRow(item: { id: any; }) {
+    this.hoveredItem = item.id
+  }
+
+  public clickRow(item: { id: any; }) {
+    this.selectedReservation = item.id;
+  }
 
 }
