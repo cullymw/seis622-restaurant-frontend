c4acf3b90aa47bdc373e28e443107be82b680bda
ellaswysell <66079270+ellaswysell@users.noreply.github.com>
2022-12-11 08:07:55 -0600

    adding comments

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -31,7 +31,7 @@ export class ReservationComponent implements OnInit {
   ) { }
 
 
-
+  // call get endpoint on page load for both available time list and reservation list
   ngOnInit(): void {
     this.dataLoading = true;
     this.reservationService.fetchReservationData()
@@ -39,7 +39,7 @@ export class ReservationComponent implements OnInit {
         next: (data) => {
           this.availableTimes = data.available;
           this.reservedTimes = data.reservations;
-          this.reservedTimes.sort((a, b) => a.id - b.id);
+          this.reservedTimes.sort((a, b) => a.id - b.id); // sort reservations by time
           this.dataLoading = false;
         },
         error: (err) => {
@@ -63,15 +63,17 @@ export class ReservationComponent implements OnInit {
       }),
     };
     
+    // created modified item with new max party number
     let modifiedAvailableItem = JSON.parse(JSON.stringify(this.selectedReservation));
     let newNum = modifiedAvailableItem.number - ngForm.value.partyNumber;
     modifiedAvailableItem.number = newNum;
 
+    // create reserved item to add to confirmed reservation list
     let reservedItem = JSON.parse(JSON.stringify(this.selectedReservation));
     reservedItem.name = ngForm.value.personName;
     reservedItem.number = ngForm.value.partyNumber;
 
-
+    // calls PUT endpoint to update available times item to modified item with new item number
     this.httpClient.put<any>("http://localhost:8080/reservation", JSON.stringify(modifiedAvailableItem), options)
         .subscribe({
           next: () => {
@@ -82,6 +84,7 @@ export class ReservationComponent implements OnInit {
           }
         });
 
+      // calls POST endpoint to add reserved item to confirmed reservation list
       this.httpClient.post<any>("http://localhost:8080/reservation", JSON.stringify(reservedItem), options)
       .subscribe({
           next: () => {
@@ -92,8 +95,10 @@ export class ReservationComponent implements OnInit {
           }
         });
 
+      // if the new max party number is 0, call DELETE endpoint to remove from available times list
       if (modifiedAvailableItem.number <= 0) 
       {
+        // delete endpoint needs it's body inside the options paramater
         const deleteOptions = {
           headers: new HttpHeaders({
             'Content-Type': 'application/json',
@@ -113,15 +118,17 @@ export class ReservationComponent implements OnInit {
       }
 
 
+    //clear the form and the selected reservation
     ngForm.resetForm();
     this.selectedReservation = {id: 0, name: "", time: "", number: 0};
 
+    //reload the available times and the reservation list
     this.reservationService.fetchReservationData()
       .subscribe({
         next: (data) => {
           this.availableTimes = data.available;
           this.reservedTimes = data.reservations;
-          this.reservedTimes.sort((a, b) => a.id - b.id);
+          this.reservedTimes.sort((a, b) => a.id - b.id); // sort by time
           this.dataLoading = false;
         },
         error: (err) => {
@@ -133,10 +140,12 @@ export class ReservationComponent implements OnInit {
 
   public hoveredItem: any;
 
+  // to highlight the row you are hovering
   public highlightRow(item: IReservationItem) {
     this.hoveredItem = item.id
   }
 
+  // clicking on an available time makes it the selectedReservation and set the party number array for the form
   public clickRow(item: IReservationItem) {
     this.selectedReservation = item;
     this.partyArray = Array.from(Array(item.number),(x,i)=>i+1);
